c311ab74f6af9b01280dad62eeefb984c792b4f9
Bailsnob <tejaskohli@gmail.com>
2024-10-28 14:33:18 -0400

    removed instructions and documentation for redundancy

--- app/documentation/page.js
+++
@@ -1,8 +0,0 @@
-export default function Documentation() {
-  return (
-    <>
-      <h1>Documentation:</h1>
-      <p> I began this project in early 2022, and, I admit, I did not know when I started how long it was going to take.</p>
-    </>
-  );
-}
